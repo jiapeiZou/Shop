3ec45c91668064996027d6ae018feac6925f9f8a
jiapei zou <jiapeizou@jiapeideMBP.lan>
2023-08-05 14:44:00 +0800

    放大镜--实现大图显示及其优化

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -21,15 +21,22 @@ const enter = (index) => {
 }
 
 // 2. 获取鼠标相对位置
-const target = ref(null) // 获取 滑块所在的容器的 DOM 元素 。并将其初始化为 null。
-const { elementX, elementY, isOutside } = useMouseInElement(target)
+const target = ref(null) // 获取 滑块所在的容器的 DOM 元素 。创建了一个可响应的引用，并将其初始化为 null。
+const { elementX, elementY, isOutside } = useMouseInElement(target) // 解构  并传入一个你想要跟踪的元素（ref类型）。 useMouseInElement 返回响应性引用 
 
 // 3.控制滑块跟随鼠标移动 （监听elementX/Y变化，一旦变化 重置left/top)
+// 小图坐标
 const left = ref(0)
 const top = ref(0)
+// 大图坐标
+const positionX = ref(0)
+const positionY = ref(0)
+// 监听鼠标移动
+watch( [elementX, elementY], () => { 
+  // 如果鼠标没有移入盒子里 ，则后续代码不执行。直接return
+  if(isOutside.value ) return 
 
-watch( [elementX, elementY], () => {
-  // 有效范围内控制滑块距离
+  // 【有效范围内控制滑块距离】
   // 横向
   if(elementX.value > 100 && elementX.value < 300) {
     left.value = elementX.value - 100 // (100 = 滑块宽度的1/2)
@@ -38,27 +45,29 @@ watch( [elementX, elementY], () => {
   if(elementY.value > 100 && elementY.value < 300) {
     top.value = elementY.value - 100 // (100 = 滑块宽度的1/2)
   }
-
-  // 边界处理
+  // 【边界处理】
   // 横向
   if(elementX.value < 100 ) {left.value = 0}
   if(elementX.value > 300 ) {left.value = 200}
   // 纵向
   if(elementY.value < 100 ) {top.value = 0}
   if(elementY.value > 300 ) {top.value = 200}
+
+  // 4.控制大图的显示(小图的两倍 并反向移动)
+  positionX.value = -left.value * 2
+  positionY.value = -top.value * 2
+
 })
 
 </script>
 
 <template>
-    X:{{ elementX }}Y:{{ elementY }}{{ isOutside }}
-    left{{ left }} top {{ top }}
     <div class="product-img">
         <!-- 左侧大图 -->
         <div class="middle" ref="target">
             <img :src="imageList[imgIndex]" alt="">
              <!-- 蒙层小滑块 (内联样式)-->
-            <div class="layer" :style="{ left:`${left}px`, top:`${top}px`}"></div>
+            <div class="layer" v-show="!isOutside" :style="{ left:`${left}px`, top:`${top}px`}"></div>
         </div>
 
         <!-- 右侧小图列表 （获取下标  激活状态） -->
@@ -74,7 +83,14 @@ watch( [elementX, elementY], () => {
         </ul>
 
         <!-- 放大镜图 --> 
-        <div class="large">
+        <div class="large" 
+            v-show="!isOutside"
+            :style="[{
+                backgroundImage:`url(${imageList[imgIndex]})`,
+                backgroundPositionX:`${positionX}px`,
+                backgroundPositionY:`${positionY}px`
+                }]"
+        >
 
         </div>
     </div>
@@ -91,19 +107,21 @@ watch( [elementX, elementY], () => {
         height:400px;
         background: #f5f5f5;
     }
-    // .large{
-    //     position: absolute;
-    //     top: 0;
-    //     left: 412px;
-    //     width:400px;
-    //     height:400px;
-    //     z-index: 500;
-    //     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    //     background-repeat: no-repeat;
-    //     // 背景图 ： 盒子大小 = 2；1 将来控制背景图的移动来实现放大的效果查看 background-position
-    //     background-size:800px 800px ;
-    //     background-color: #f8f8f8;
-    // }
+
+    // 放大镜图
+    .large{
+        position: absolute;
+        top: 0;
+        left: 412px;
+        width:400px;
+        height:400px;
+        z-index: 500;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        background-repeat: no-repeat;
+        // 背景图 ： 盒子大小 = 2；1 将来控制背景图的移动来实现放大的效果查看 background-position
+        background-size:800px 800px ;
+        background-color: #f8f8f8;
+    }
 
     .layer{
         width:200px;
